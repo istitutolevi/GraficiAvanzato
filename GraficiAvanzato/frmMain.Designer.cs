de410eaecbbdbf7d56292e2e9339050443b840c4
bubibubi <u.ballestrazzi@gmail.com>
2020-10-06 19:03:57 +0200

    Add project files.

---
+++ GraficiAvanzato/frmMain.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace GraficiAvanzato
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.txtMinX = new System.Windows.Forms.TextBox();
+            this.txtMaxX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMinY = new System.Windows.Forms.TextBox();
+            this.txtMaxY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDisegna = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct
+            // 
+            this.pct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pct.Location = new System.Drawing.Point(0, 0);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(800, 450);
+            this.pct.TabIndex = 0;
+            this.pct.TabStop = false;
+            // 
+            // txtMinX
+            // 
+            this.txtMinX.Location = new System.Drawing.Point(44, 8);
+            this.txtMinX.Name = "txtMinX";
+            this.txtMinX.Size = new System.Drawing.Size(100, 20);
+            this.txtMinX.TabIndex = 1;
+            // 
+            // txtMaxX
+            // 
+            this.txtMaxX.Location = new System.Drawing.Point(163, 8);
+            this.txtMaxX.Name = "txtMaxX";
+            this.txtMaxX.Size = new System.Drawing.Size(100, 20);
+            this.txtMaxX.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "X";
+            // 
+            // txtMinY
+            // 
+            this.txtMinY.Location = new System.Drawing.Point(44, 34);
+            this.txtMinY.Name = "txtMinY";
+            this.txtMinY.ReadOnly = true;
+            this.txtMinY.Size = new System.Drawing.Size(100, 20);
+            this.txtMinY.TabIndex = 5;
+            // 
+            // txtMaxY
+            // 
+            this.txtMaxY.Location = new System.Drawing.Point(163, 34);
+            this.txtMaxY.Name = "txtMaxY";
+            this.txtMaxY.ReadOnly = true;
+            this.txtMaxY.Size = new System.Drawing.Size(100, 20);
+            this.txtMaxY.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Y";
+            // 
+            // btnDisegna
+            // 
+            this.btnDisegna.Location = new System.Drawing.Point(302, 6);
+            this.btnDisegna.Name = "btnDisegna";
+            this.btnDisegna.Size = new System.Drawing.Size(75, 23);
+            this.btnDisegna.TabIndex = 8;
+            this.btnDisegna.Text = "Disegna";
+            this.btnDisegna.UseVisualStyleBackColor = true;
+            this.btnDisegna.Click += new System.EventHandler(this.btnDisegna_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDisegna);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMaxY);
+            this.Controls.Add(this.txtMaxX);
+            this.Controls.Add(this.txtMinY);
+            this.Controls.Add(this.txtMinX);
+            this.Controls.Add(this.pct);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.TextBox txtMinX;
+        private System.Windows.Forms.TextBox txtMaxX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMinY;
+        private System.Windows.Forms.TextBox txtMaxY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDisegna;
+    }
+}
+
---
+++ GraficiAvanzato/frmMain.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NCalc;
+
+namespace GraficiAvanzato
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        private void btnDisegna_Click(object sender, EventArgs e)
+        {
+            double minX = double.Parse(txtMinX.Text);
+            double maxX = double.Parse(txtMaxX.Text);
+
+            double stepX = (maxX - minX) / pct.Width;
+
+            double minY;
+            double maxY;
+
+
+            CalcolaMinMaxY(minX, maxX, stepX, out minY, out maxY);
+
+            txtMinY.Text = minY.ToString();
+            txtMaxY.Text = maxY.ToString();
+
+            double scaleX = (maxX - minX) / pct.Width;
+            double scaleY = (maxY - minY) / pct.Height;
+
+            Pen pen = new Pen(Color.Blue);
+            Graphics graphics = pct.CreateGraphics();
+            for (double x = minX; x <= maxX; x += stepX)
+            {
+                double y = F(x);
+
+                double cX = (x - minX) / scaleX;
+                double cY = (y - minY) / scaleY;
+
+                graphics.DrawRectangle(pen, (float)cX, (float)cY, 1, 1);
+            }
+            graphics.Dispose();
+
+        }
+        */
+
+
+        private void btnDisegna_Click(object sender, EventArgs e)
+        {
+            double minX = double.Parse(txtMinX.Text);
+            double maxX = double.Parse(txtMaxX.Text);
+
+            double stepX = (maxX - minX) / pct.Width;
+
+            double minY;
+            double maxY;
+
+
+            CalcolaMinMaxY(minX, maxX, stepX, out minY, out maxY);
+
+            txtMinY.Text = minY.ToString();
+            txtMaxY.Text = maxY.ToString();
+
+            double scaleX = (maxX - minX) / pct.Width;
+            double scaleY = (maxY - minY) / pct.Height;
+
+            Pen pen = new Pen(Color.Blue);
+            Graphics graphics = pct.CreateGraphics();
+
+            double oldX = minX;
+            double oldY = F(minX);
+
+            for (double x = minX; x <= maxX; x += stepX)
+            {
+                double y = F(x);
+
+                double cX = (x - minX) / scaleX;
+                double cY = (y - minY) / scaleY;
+
+                double cOldX = (oldX - minX) / scaleX;
+                double cOldY = (oldY - minY) / scaleY;
+
+                graphics.DrawLine(pen, (float)cOldX, (float)cOldY, (float)cX, (float)cY);
+
+                oldX = x;
+                oldY = y;
+            }
+            graphics.Dispose();
+
+        }
+
+
+        private void CalcolaMinMaxY(double minX, double maxX, double stepX, out double minY, out double maxY)
+        {
+            minY = F(minX);
+            maxY = F(minX);
+
+            for (double x = minX; x <= maxX; x += stepX)
+            {
+                double y = F(x);
+                if (y < minY)
+                    minY = y;
+                if (y > maxY)
+                    maxY = y;
+            }
+        }
+
+
+        private double F(double x)
+        {
+            return x * Math.Sin(x);
+        }
+
+    }
+}
